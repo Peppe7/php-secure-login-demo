69fb1867593499790e6b906da48cf5153a3de8c0
Peppe7 <32358605+Peppe7@users.noreply.github.com>
2025-05-07 23:39:55 +0200

    Update init.sql

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,1 +1,10 @@
+CREATE TABLE IF NOT EXISTS users(
+  id INT AUTO_INCREMENT PRIMARY KEY
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
 
+INSERT INTO users(username, password) VALUES (
+  'admin',
+  '$2y$10$e0NRoNznf2Qu2z7qeRul2eV8WQCFyMWhxDCDOJKI/p.SIHV/mfNGq'
+);
