244b0b61595495d31cbef03c78ca3c1aec9362a7
Peppe7 <32358605+Peppe7@users.noreply.github.com>
2025-05-07 23:30:40 +0200

    Update index.php

--- secure/index.php
+++ secure/index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Login Insicuro</title>
-    </head>
-    <body>
-        <h2>Login (Versione Insicura)</h2>
-        <form method="post" action="login.php">
-            <input type="text" name="username" placeholder="Username"><br><br>
-            <input type="text" name="password" placeholder="Password"><br><br>
-            <input type="submit" value="Login"><br><br>
-        </form>
-    </body>
+<head>
+    <title>Login Insicuro</title>
+</head>
+<body>
+    <h2>Login (Versione Insicura)</h2>
+    <form method="post" action="login.php">
+        <input type="text" name="username" placeholder="Username"><br><br>
+        <input type="password" name="password" placeholder="Password"><br><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
 </html>
